4d6449deca6de72782504041d3f3759e857892f1
ithai5 <ithai.g@gmail.com>
2025-02-09 15:19:05 +0100

    Adjust solution

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -15,37 +15,55 @@ const IconInput = ({
 }) => {
 
   const fontSize = size === "small" ? 1 : 1.5
-  return <Wrapper style={{ "--width":`${width}px`, "--font-size": `${fontSize}rem` }}>
+  return <Wrapper style={{"--font-size" :`${fontSize}rem` }}>
     <label htmlFor={label}>
     <IconWrapper>
     <Icon id={icon} size={fontSize*16}></Icon></IconWrapper>
-    <Input type="text" name={label} placeholder={placeholder} style={{"--font-size":`${fontSize}rem`}} size={size}/></label>
+    <Input type="text" name={label} placeholder={placeholder} style={{"--font-size":`${fontSize}rem` , "--width":`${width}px`}} size={size}/></label>
   </Wrapper>
 };
 
-const Wrapper = styled.div`
-  width: var(--width);
+const Wrapper = styled.label`
+  display: inline-block;
   font-size: var(--font-size);
+  color: ${COLORS.gray700};
   position:relative;
+  &:hover {
+      color: ${COLORS.black};
+  }
 `
 
 const Input = styled.input`
-  width: 100%;/*var(--width);*/
+  //width: 100%;/*var(--width);*/
   border: none;
   border-bottom: 1px solid ${COLORS.black};
   font-size: var(--font-size);
+  width: var(--width);
+  
+  outline-offset: 2px;
   &[type="text"] {
     padding-left: ${props => (props.size === 'small' ? `18px` : '30px')};
-    18px
-  };
     font-size: var(--font-size);
+    font-weight: 700;
+    };
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+
+/*    &::placeholder {
+      color: ${COLORS.black};
+      font-weight: 400;
+    }*/
   }
 `
 const IconWrapper = styled.div`
+  color: inherit;
   position: absolute;
   top: 2px;
   bottom: 0;
   left: 0;
+  pointer-events: none;
 `
 
 export default IconInput;
